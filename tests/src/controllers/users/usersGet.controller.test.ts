75db9285eaed78fa7e79eb5075c9a5cb6a038317
Domingo Llanes <domingollanes.dev@gmail.com>
2022-04-03 13:21:31 +0200

    feat: add unitary test for controller

--- tests/src/controllers/users/usersGet.controller.test.ts
+++ tests/src/controllers/users/usersGet.controller.test.ts
@@ -1,17 +1,28 @@
+import { Request, Response } from 'express';
 import { UserRepository } from '../../../../src/repositories/user.repository';
 import UsersGetController from '../../../../src/controllers/users/usersGet.controller';
 
-jest.mock('../../../../src/repositories/user.repository');
-jest.mock('../../../../src/controllers/users/usersGet.controller');
+const userRepository: jest.Mocked<UserRepository> = {
+  find: jest.fn(),
+  findOneByEmail: jest.fn(),
+}
 
 describe('usersGet controller', () => {
+  it('Should call to repository and returns 200 code', async () => {
+    let responseStatus;
+    const req = {} as Request;
+    const res: Partial<Response> = {
+      send: jest.fn(),
+      status: jest.fn().mockImplementation((statusCode) => {
+        responseStatus = statusCode;
+        return res;
+      }),
+    };
 
-  it('Should call to repository', () => {
-    const req = {},
-          res = { send: jest.fn() };
+    const userController = new UsersGetController(userRepository);
+    await userController.run(req, res as Response);
 
-    const userController = new UsersGetController();
-    userController.run(req, res)
-    expect(UserRepository).toBeCalled();
+    expect(userRepository.find).toBeCalled();
+    expect(responseStatus).toEqual(200);
   });
 });
